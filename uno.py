818a7a9dbd88b27e82ee204e064d8aa682cab7c7
Helper0x <Helper0x@users.noreply.github.com>
2024-02-01 11:51:17 +0100

    random

--- uno.py
+++ uno.py
@@ -1,85 +1,91 @@
 # 26.01.24 -> 1.02.24
 
 # Class import
-from Class.carta import Carta
+from Class.carta import Carta, AVAILABLE_COLORS
 from Class.mazzo import Mazzo
 from Class.mano import Mano
 
+# General import
+import time, random
+
 # Variabili
 numero_player = 3
+giocatori = ['uno', 'due', 'tre']
 
 def nuova_posizione(lista, posizione_attuale, spostamento):
-    if 0 <= posizione_attuale <= len(lista):
-        return (posizione_attuale + spostamento) % len(lista)
+    return (posizione_attuale + spostamento) % len(lista)
 
 def cambia_giro_invertito(lista_giocatori, index_giocatore_selezionato):
     nuova_lista_giocatori = lista_giocatori[index_giocatore_selezionato:] + lista_giocatori[:index_giocatore_selezionato]
     return nuova_lista_giocatori[::-1]
 
-def fai_mossa(index_turno: int, mazzo_iniziale: Mazzo, mazzi_giocatori: Mano):
+def logic_stategia(id_giocatore, carta_centro, lista_carte_valide, len_carte_next_player, get_colore):
+    id_player = id_giocatore.split("_")[1]
+    is_re = (id_player == "uno")
 
-    print("\n####################")
-    print(f"-> Id giocatore: {mazzi_giocatori[index_turno].id_giocatore}")
-    carta_al_centro = mazzo_iniziale.carta_al_centro
-    print(f"-> Carta al centro: {carta_al_centro.to_string()}")
-    print(f"-> Carte giocatore: {mazzi_giocatori[index_turno].to_string(False)}")
-    print(f"-> Carte valide: {mazzi_giocatori[index_turno].get_carte_valide(carta_al_centro)}")
+    # Per colore
+    if get_colore:
+        return random.choice(AVAILABLE_COLORS)
 
-    indice_carta_scelta = input("Scegli una carta: -1(pesca): ")
-    
-    if indice_carta_scelta.isdigit() or indice_carta_scelta == "-1":
-        indice_carta_scelta = int(indice_carta_scelta)
-
-        if -1 <= indice_carta_scelta and indice_carta_scelta <= len(mazzi_giocatori[index_turno].player)-1:
-
-            # Salta
-            if indice_carta_scelta == -1:
-                carta_random = mazzo_iniziale.get_carta_random()
-                print(f"-> Carta random: {carta_random.__dict__}")
-                mazzi_giocatori[index_turno].player.append(carta_random)
-
-            # Carta
-            else:
-
-                # Se la carta è valida
-                carta_scelta = mazzi_giocatori[index_turno].player[indice_carta_scelta]
-                if mazzi_giocatori[index_turno].is_carta_possibile(carta_scelta, carta_al_centro):
-                    ss_return = ""
-
-                    if carta_scelta.tipo == "+4":
-                        ss_return = "carte+4"
-
-                    elif carta_scelta.tipo == "+2":
-                        ss_return = "carte+2"
-                    
-                    elif carta_scelta.tipo == "new_colore":
-                        nuovo_colore_scelto = input(f"Scelgi colore tra [r, g, v, b]: ")
-                        mazzo_iniziale.carta_al_centro = Carta(nuovo_colore_scelto, None)
-
-                    elif carta_scelta.tipo == "divieto":
-                        ss_return = "giocatori+1"
-
-                    elif carta_scelta.tipo == "cambio_giro":
-                        ss_return = "giocatore_inverti"
-                    
-                    else:
-                        mazzo_iniziale.carta_al_centro = carta_scelta
-
-                    # Rimuovi carta dal mazzo del giocatore
-                    mazzi_giocatori[index_turno].delete_card(carta_scelta, mazzo_iniziale)
-                    return ss_return
-
-                else:
-                    print("Carta non valida")
-                    fai_mossa(index_turno, mazzo_iniziale, mazzi_giocatori)
+    # Per carte
+    else:
+        if len(lista_carte_valide) == 0:
+            return -1
 
         else:
-            print("Valore carta fuori dal numero di carte")
-            fai_mossa(index_turno, mazzo_iniziale, mazzi_giocatori)
+            return random.choice(lista_carte_valide)
 
+def fai_mossa(index_turno: int, mazzo_iniziale: Mazzo, mazzi_giocatori: Mano):
+
+    id_giocatore = mazzi_giocatori[index_turno].id_giocatore
+    carta_al_centro = mazzo_iniziale.carta_al_centro
+    carte_valide = mazzi_giocatori[index_turno].get_carte_valide(carta_al_centro)
+
+    print("\n####################")
+    print(f"-> Id giocatore: {id_giocatore}")
+    print(f"-> Carta al centro: {carta_al_centro.to_string()}")
+    print(f"-> Carte giocatore: {mazzi_giocatori[index_turno].to_string(False)}")
+    print(f"-> Carte valide: {carte_valide}")
+
+    # Scelta della carta random
+    indice_carta_scelta = logic_stategia(id_giocatore, carta_al_centro, carte_valide, len(mazzi_giocatori[(index_turno+2) % len(giocatori)].player), False)
+
+    # Salta
+    print(f"-> Carte scelta: {indice_carta_scelta}")
+    if indice_carta_scelta == -1:
+        carta_random = mazzo_iniziale.get_carta_random()
+        print(f"-> Carta random: {carta_random.__dict__}")
+        mazzi_giocatori[index_turno].player.append(carta_random)
+
+    # Carta
     else:
-        print("Valore input sbagliato")
-        fai_mossa(index_turno, mazzo_iniziale, mazzi_giocatori)
+
+        # Se la carta è valida
+        carta_scelta = mazzi_giocatori[index_turno].player[indice_carta_scelta]
+        ss_return = ""
+
+        if carta_scelta.tipo == "+4":
+            ss_return = "carte+4"
+
+        elif carta_scelta.tipo == "+2":
+            ss_return = "carte+2"
+                    
+        elif carta_scelta.tipo == "new_colore":
+            nuovo_colore_scelto = logic_stategia(id_giocatore, carta_al_centro, carte_valide, len(mazzi_giocatori[(index_turno+2) % len(giocatori)].player), True)
+            mazzo_iniziale.carta_al_centro = Carta(nuovo_colore_scelto, None)
+
+        elif carta_scelta.tipo == "divieto":
+            ss_return = "giocatori+1"
+
+        elif carta_scelta.tipo == "cambio_giro":
+            ss_return = "giocatore_inverti"
+                    
+        else:
+            mazzo_iniziale.carta_al_centro = carta_scelta
+
+        # Rimuovi carta dal mazzo del giocatore
+        mazzi_giocatori[index_turno].delete_card(carta_scelta, mazzo_iniziale)
+        return ss_return
 
     print("\n")
 
@@ -92,7 +98,7 @@ def main():
 
     # Creazione lista con giocatori
     mazzi_giocatori = []
-    for nomi in ['uno', 'due', 'tre']:
+    for nomi in giocatori:
         mazzi_giocatori.append(Mano(mazzo_iniziale, f"giocatore_{nomi}"))
 
     # Inizio
@@ -104,6 +110,7 @@ def main():
         if len(mazzi_giocatori[index_turno].player) == 0:
             print("\nFINE")
             print(mazzi_giocatori[index_turno].id_giocatore)
+            return mazzi_giocatori[index_turno].id_giocatore
             break
 
         if return_mossa == "giocatori+1":
@@ -128,5 +135,16 @@ def main():
                     carta_random = mazzo_iniziale.get_carta_random()
                     print(f"-> Carta random: {carta_random.__dict__}, aggiunta al mazzo: {index_turno}")
                     mazzi_giocatori[index_turno].player.append(carta_random)
+
+
+def statistica():
+    
+    stat = {nome:0 for nome in giocatori}
+    for i in range(1000):
+        print(f"Gioco: {i}")
+        result_gioco = main().split("_")[1]
+        stat[result_gioco] += 1
+
+    print(stat)
 
 main()
